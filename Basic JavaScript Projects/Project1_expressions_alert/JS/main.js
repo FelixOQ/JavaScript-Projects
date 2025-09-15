a8d539ce07da71c6c7222338e6afcffa9848e94b
Felix <felixoliverquinn@gmail.com>
2025-09-15 20:01:50 +0100

    Added index.html and main.js to Project1_expressions_alert folder to practice new JavaScript features such as declaring variables, concatenating strings, window.alert(), document.write(), and commenting code.

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,31 @@
+window.alert("Hello, World!");  //Makes a pop-up appear with the words "Hello, World!" and an OK button
+
+document.write('Hello, World!');    //Writes "Hello, World!" directly into the HTML document
+
+var F = "First string";     //Declares the variable "F" and assigns it the string value "First string"
+
+document.write(F);      //Writes "First string" (the value of the variable F) directly into the HTML document
+
+window.alert(F);    //Makes a pop-up appear with the value of the F variable and an OK button
+
+var S = "Don't tell me this \"loser\" is demanding compensation";   //Declares the variable S and assigns it a string value which incorporates escape characters
+
+document.write(S);  //Writes the value of the S variable directly into the HTML document
+
+document.write("<br>" + F + "<br>" + S);    //Writes line breaks concatenated with the values of the F and S variables directly into the HTML document
+
+var C = "Here's a " + "concatenated" + " string."   //Declares the variable C and assigns it a concatenated string value
+
+document.write("<br>" + C + "<br>");    //Writes line breaks concatenated with the value of the C variable directly into the HTML document
+
+var Wizard = "Peregrine", Cleric = "Olive", Bard = "Duk", Monk = "Winter", Ranger = "Nom";      //Declares several variables in the same statement and assigns them each a value
+
+document.write(Cleric);     //Writes "Olive" (the value of the variable "Cleric") directly into the HTML document
+
+var x = 14  //Declares the variable x and assigns it the integer value 14
+
+document.write(x + 7);  //Writes "21" (the result of the expression x + 7) directly into the HTML document
+
+var Sent1 = "This is the beginning of the string", Sent2 = " and this is the end of the string";    //Declares two variables and assigns them string values
+
+document.write("<br>" + Sent1 + Sent2);     //Writes a line break concatenated with variables Sent1 and Sent2 directly into the HTML document
